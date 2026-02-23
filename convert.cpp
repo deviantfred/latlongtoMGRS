bb4ce3ed147dd5a6642bbea953a19fb2d75274a6
deviantfred <freddeviant@proton.me>
2026-02-23 14:35:04 -0600

    utm easting initial completed

--- convert.cpp
+++ convert.cpp
@@ -17,18 +17,31 @@ struct mgrs{
     int northing;
 };
 
-int calcUTMeast(double lat, double longitude){
-    double N = EqatorialRadius/sqrt(1-EccentricitySquared*sin(lat*3.141592658979/180)^2)
+double calcUTMeast(double lat, double longitude){
+    int utmzone = int((longitude+180)/6)+1;
+    double CentralMeridian = (utmzone-1)*6-180+3;
+    double N = EqatorialRadius / sqrt(1-EccentricitySquared*pow(sin(lat*3.141592658979/180),2.0));
+    double T = pow(tan(lat*3.141592658979/180), 2.0);
+    double C = pow(EccentricitySquared * cos(lat*3.141592658979/180),2.0);
+    double A = cos(lat*3.141592658979/180)*(longitude-CentralMeridian)*3.141592658979/180;
+    double M = EqatorialRadius*((1-EccentricitySquared/4-3*pow(EccentricitySquared,2.0)/64-5*pow(EccentricitySquared,3.0)/256)*lat*3.141592658979/180-(3*EccentricitySquared/8+3*pow(EccentricitySquared,2.0)/32+45*pow(EccentricitySquared,3.0)/1024)*sin(2*lat*3.141592658979/180)+(15*pow(EccentricitySquared,2.0)/256+45*pow(EccentricitySquared,3.0)/1024)*sin(4*lat*3.141592658979/180)-(35*pow(EccentricitySquared,3.0)/3072)*sin(6*lat*3.141592658979/180));
+    double utmeast = ScaleFactor*N*(A+(1-T+C)*pow(A,3.0)/6+(5-18*T+pow(T,2.0)+72*C-58*EccentricitySquared)*pow(A,5.0)/120)+500000;
+    return utmeast;
+}
+
+double calcUTMnorth(double lat, double long){
+    
 }
 
 
-
-int main(double lat, double longitude){
+int main(){
     mgrs coords;
     coords.squareid = "test";
+    //test coords mgrs for it is: 15R VQ 90816 46052  UTM:15R 490816.69 3446052.01
+    double lat = 31.14839523291297;
+    double longitude = -93.09634421104477;
     
-    
-    
+    printf("%f\n", calcUTMeast(lat, longitude));
     
     
     return 0;
