0319589a2b49bcb38349531200e5344a2568285f
root <root@DESKTOP-7CU1T89.localdomain>
2026-02-23 10:42:30 -0600

    Committer: root <root@DESKTOP-7CU1T89.localdomain>  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   convert.cpp
    	new file:   test.out

--- convert.cpp
+++ convert.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <string>
+#include <math.h>
 
+using namespace std;
+
+struct mgrs{
+    string gsd;
+    string sqareid;
+    int easting;
+    int northing;
+};
+
+//function to find the grid zone
+int findGridZone(float lat , float longitude){
+    int zoneNum = (longitude + 180) / 2;
+    
+
+
+
+}
+
+
+
+// convert the lat long into mgrs coords
+int latlongtoMGRS(mgrs coords, float lat, float longitude){
+    
+
+    cout << "MGRS: " << mgrs.gsd << " " << mgrs.sqareid << " " << mgrs.easting << " " << mgrs.northing << endl;
+
+    return 0;
+}
+
+
+
+
+int main(float lat, float longitude){
+    mgrs coords;
+
+    latlongtoMGRS(coords, lat, longitude);
+    
+    
+    
+    
+    
+    return 0;
+}
