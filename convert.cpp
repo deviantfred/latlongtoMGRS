8b82c54e7209d5ffc7f3dbf5eb47855ddd0075b0
deviantfred <freddeviant@proton.me>
2026-02-23 15:53:47 -0600

    Well it turns out if you're supposed to cast something as an int, you should do that... My brain is smooth AF

--- convert.cpp
+++ convert.cpp
@@ -39,7 +39,7 @@ void calcBackground (double lat, double longitude, double arr[7]){
 
 //function to calclate the easting
 double calcUTMeast(double lat, double longitude, double arr[7]){
-    
+    /*
     int utmzone = int((longitude+180)/6)+1;
     double CentralMeridian = (utmzone-1)*6-180+3;
     double N = EqatorialRadius / sqrt(1-EccentricitySquared*pow(sin(lat*3.141592658979/180),2.0));
@@ -47,8 +47,8 @@ double calcUTMeast(double lat, double longitude, double arr[7]){
     double C = pow(EccentricitySquared * cos(lat*3.141592658979/180),2.0);
     double A = cos(lat*3.141592658979/180)*(longitude-CentralMeridian)*3.141592658979/180;
     double M = EqatorialRadius*((1-EccentricitySquared/4-3*pow(EccentricitySquared,2.0)/64-5*pow(EccentricitySquared,3.0)/256)*lat*3.141592658979/180-(3*EccentricitySquared/8+3*pow(EccentricitySquared,2.0)/32+45*pow(EccentricitySquared,3.0)/1024)*sin(2*lat*3.141592658979/180)+(15*pow(EccentricitySquared,2.0)/256+45*pow(EccentricitySquared,3.0)/1024)*sin(4*lat*3.141592658979/180)-(35*pow(EccentricitySquared,3.0)/3072)*sin(6*lat*3.141592658979/180));
-    
-    double utmeast = ScaleFactor * (arr[2]) * ((arr[5]) + (1 - (arr[3]) + (arr[4])) * pow((arr[5]),3.0) / 6 + (5 - 18 * (arr[3]) + pow((arr[3]),2.0) + 72 * (arr[4]) - 58 * EccentricitySquared) * pow((arr[5]),5.0) / 120) + 500000;
+    */
+    double utmeast = ScaleFactor * arr[2] * (arr[5] + (1 - arr[3] + arr[4]) * pow(arr[5],3.0) / 6 + (5 - 18 * arr[3] + pow(arr[3],2.0) + 72 * arr[4] - 58 * EccentricitySquared) * pow(arr[5],5.0) / 120) + 500000;
     return utmeast;
 }
 
@@ -81,7 +81,21 @@ int main(){
     
     //array for background calculations
     double backgroundarr[7]; //utmzone, meridian, N, T, C, A, M
-    calcBackground(lat, longitude, backgroundarr);
+     //utmzone
+    backgroundarr[0] = int((longitude+180)/6)+1;
+    //centralMeridian    
+    backgroundarr[1] = (backgroundarr[0]-1)*6-180+3;
+    //N
+    backgroundarr[2] = EqatorialRadius / sqrt(1-EccentricitySquared*pow(sin(lat*3.141592658979/180),2.0));
+    //T
+    backgroundarr[3] = pow(tan(lat*3.141592658979/180), 2.0);
+    //C
+    backgroundarr[4] = pow(EccentricitySquared * cos(lat*3.141592658979/180),2.0);
+    //A
+    backgroundarr[5] = cos(lat*3.141592658979/180)*(longitude-backgroundarr[1])*3.141592658979/180;
+    //M
+    backgroundarr[6] = EqatorialRadius*((1-EccentricitySquared/4-3*pow(EccentricitySquared,2.0)/64-5*pow(EccentricitySquared,3.0)/256)*lat*3.141592658979/180-(3*EccentricitySquared/8+3*pow(EccentricitySquared,2.0)/32+45*pow(EccentricitySquared,3.0)/1024)*sin(2*lat*3.141592658979/180)+(15*pow(EccentricitySquared,2.0)/256+45*pow(EccentricitySquared,3.0)/1024)*sin(4*lat*3.141592658979/180)-(35*pow(EccentricitySquared,3.0)/3072)*sin(6*lat*3.141592658979/180));
+
 
     
     
